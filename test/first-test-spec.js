3cf8d6b5318845027263dea117b69cc48504d432
Fengda HUANG <h@phodal.com>
2015-12-13 01:03:34 +0800

    A simple diff test

--- test/first-test-spec.js
+++ test/first-test-spec.js
@@ -8,4 +8,22 @@ describe("A suite", function () {
       expect($(virtualDom.create(eval(hscript))).html()).toBe($(fixtures).html());
     });
   });
+  it("should different", function () {
+    var parser = require('html2hscript');
+    var vDOM = virtualDom;
+    var h = vDOM.h;
+
+    var originDOM = '<div id="test"><div id="example"></div><h1 class="hello">Hello World</h1></div></div>';
+    var changeDOM = '<div id="test"><div id="example"></div><h1 class="world">Hello World</h1></div></div>';
+
+    var result, expected;
+    parser(originDOM, function (err, hscript) {
+      result = eval(hscript);
+    });
+    parser(changeDOM, function (err, hscript) {
+      expected = eval(hscript);
+    });
+    var patches = vDOM.diff(result, expected);
+    expect(patches[2].patch.className).toBe("world");
+  });
 });
