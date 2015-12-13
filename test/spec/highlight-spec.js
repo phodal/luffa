f3f3eafaa049e71122fae989adf6523df322ae29
Fengda HUANG <h@phodal.com>
2015-12-13 23:20:53 +0800

    Add test for simple diff

--- test/spec/highlight-spec.js
+++ test/spec/highlight-spec.js
@@ -6,7 +6,7 @@ describe("HighLight Change", function () {
     fixtures = '<div id="test"><div id="example"><h1 class="hello">Hello World</h1></div></div>';
     setFixtures(fixtures);
   });
-  it("should diff result with style change", function () {
+  it("should diff result with class change", function () {
     $(".hello").attr('class', 'hello world');
     parser($("#test").html(), function (err, hscript) {
       tmpDIV.appendChild(virtualDom.create(eval(hscript)));
@@ -14,6 +14,28 @@ describe("HighLight Change", function () {
       var className = patches[1].patch.className;
       console.log('%c ' + $(fixtures).html(), 'background: #222; color: #bada55');
       console.log('%c ' + $(tmpDIV).html().replace('class="' + className + '"', '%cclass="' + className + '"%c '), 'background: #222; color: #bada55', 'color: red', 'background: #222; color: #bada55');
+      expect(true).toBe(true);
+    });
+  });
+  it("should diff result with text change", function () {
+    $(".hello").html('World');
+    parser($("#test").html(), function (err, hscript) {
+      tmpDIV.appendChild(virtualDom.create(eval(hscript)));
+      var patches = luffa.diff($(fixtures).html(), $(tmpDIV).html(), eval(hscript));
+      var text = patches[2].patch.text;
+      console.log('%c ' + $(fixtures).html(), 'background: #222; color: #bada55');
+      console.log('%c ' + $(tmpDIV).html().replace(text, '%c' + text + '%c'), 'background: #222; color: #bada55', 'color: red', 'background: #222; color: #bada55');
+      expect(true).toBe(true);
+    });
+  });
+  it("should diff result with text change", function () {
+    $(".hello").append("<p>Test</p>" );
+    parser($("#test").html(), function (err, hscript) {
+      tmpDIV.appendChild(virtualDom.create(eval(hscript)));
+      var patches = luffa.diff($(fixtures).html(), $(tmpDIV).html(), eval(hscript));
+      var text = $(virtualDom.create(patches[1].patch)).prop("outerHTML");
+      console.log('%c ' + $(fixtures).html(), 'background: #222; color: #bada55');
+      console.log('%c ' + $(tmpDIV).html().replace($(text).prop("outerHTML"), '%c' + text + '%c'), 'background: #222; color: #bada55', 'color: red', 'background: #222; color: #bada55');
       expect(true).toBe(true);
     });
   });
