3403ffb60339fcc2860832a91923cc9d34e74684
Fengda HUANG <h@phodal.com>
2015-12-14 09:15:57 +0800

    simplify test

--- test/spec/generator-spec.js
+++ test/spec/generator-spec.js
@@ -1,21 +1,18 @@
 describe("Generator Change", function () {
-  var parser, tmpDIV, fixtures;
+  var parser, fixtures;
   beforeEach(function () {
     parser = require('html2hscript');
-    tmpDIV = document.createElement("div");
     fixtures = '<div id="test"><div id="example"><h1 class="hello">Hello World</h1></div></div>';
     setFixtures(fixtures);
   });
-  it("should diff result with class change", function () {
+  it("should return changed class", function () {
     $(".hello").attr('class', 'hello world');
     parser($("#test").html(), function (err, hscript) {
-      tmpDIV.appendChild(virtualDom.create(eval(hscript)));
-      var patches = luffa.diff($(fixtures).html(), $(tmpDIV).html(), eval(hscript));
+      var changedDOM = virtualDom.create(eval(hscript)).outerHTML;
+      var patches = luffa.diff($(fixtures).html(), changedDOM);
       var className = luffa.getDiffDom(patches)[0].prop;
-      console.log('%c ' + $(fixtures).html(), 'background: #222; color: #bada55');
-      var result = $(tmpDIV).html().replace('"' + className + '"', '%c' + className + '%c');
-      console.log('%c ' + result, 'background: #222; color: #bada55', 'color: red', 'background: #222; color: #bada55');
-      expect(true).toBe(true);
+
+      expect(className).toBe("hello world");
     });
   });
 });
